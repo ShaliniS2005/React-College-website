263e9c27530afdb6814280b77e6e97d73a64a9f3
Shalini <shaluantomable@gmail.com>
2026-07-13 20:25:21 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import Approutes from "./Routes/Approutes";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Approutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
